964ef9033cc8f877c6947cdf2586d503676efd0b
zx73 <alireza.hidden1990@gmail.com>
2016-12-17 20:34:18 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,7 +234,7 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {22298441,44487610,33347823,tonumber(our_id)},--Sudo users
+    sudo_users = {95160479,0,0,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
